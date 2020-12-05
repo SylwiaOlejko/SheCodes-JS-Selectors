32048573d6705f4d5637b1686fbf815c2c0cb795
SylwiaOlejko <63730148+SylwiaOlejko@users.noreply.github.com>
2020-12-05 01:15:50 +0100

    SheCodes-JS-selector-country

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@
 document.querySelectorAll("li#special p");
 // Challenge 2
 // Log all li with class of country
-
+let country = document.querySelectorAll("li.country");
+console.log(country);
 // Challenge 3
 // Add class special to the li with id special
-
+let special = document.querySelectorAll("li.special");
 // Challenge 4
 // In your code, replace the content of the element with id special
 // by your favorite country (other than Portugal of course)
